f047170bef456a2e382329ab8b807a37f728d1ce
Jordan Benjamin <jordanjtck@gmail.com>
2018-08-07 10:56:33 -0400

    Added proper instructions docstring, organized code.

--- buf/commands/chemical.py
+++ buf/commands/chemical.py
@@ -2,25 +2,50 @@
 # Author: Jordan Juravsky
 # Date created: 27-07-2018
 
-# TODO: add editing.
-
 import os
-
 import tabulate
-
 from sys import exit
-
 from buf import user_input
 
 instructions = """buf chemical:
 
 This subcommand allows you to access and modify your chemical library, i.e. your personal \
-list of chemicals that you use to make buffers.
+list of chemicals and their molar masses.
 
-To add a chemical to your library, call 'buf chemical -a <molar_mass> <chemical_names>...' \
-The repeating final argument allows you to specify multiple names for the same chemical.  \
-For example, calling 'buf chemical -a 58.44 NaCl salt' adds both 'NaCl' and 'salt' to your \
-chemical library, both with the same molar mass."""
+Before making buffers that specify a chemical's concentration in molar, that chemical's molar \
+mass must first be added to your chemical library. To do this, use 'buf -a <molar_mass> <chemical_names>...', where \
+the chemical's molar mass is in g/mol. For example, after adding NaCl to your library with 'buf add -a 58.44 NaCl', \
+you can then 'buf make 2L 1M NaCl', specifying the concentration of NaCl in molar.
+
+Chemicals can have multiple names, which can be listed upon addition to your library. For example, using \
+'buf chemical -a 58.44 NaCl salt' allows you use either the name 'salt' or 'NaCl' when making buffers (i.e. 'buf make 2L 1M NaCl' \
+is equivalent to 'buf make 2L 1M salt', since both expressions refer to the same molar mass. 
+
+To add additional names to an existing entry in your chemical library (also known as 'nicknaming' the chemical), use \
+'buf chemical -n <existing_chemical_name> <nicknames>...'. For example, if you added NaCl to your library with 'buf chemical \
+ -a 58.44 NaCl', and then nicknamed the chemical with 'buf chemical -n NaCl salt table_salt', you could use any of 'NaCl', 'salt', \
+ or 'table_salt' to refer to the same molar mass.
+ 
+Another way to add chemicals to your library is by specifying a list of them in a text file. This file should contain one chemical \
+per line, where each line first specifies the chemical's molar mass, followed by the list of the chemical's names. Spaces should \
+separate each item on a line. For example, if a file 'chemicals.txt' contained the following:
+
+58.44 NaCl salt
+68.08 Imidazole imi
+74.55 KCl
+
+Using 'buf chemical -a chemicals.txt' would add these three chemicals to your library. 
+
+To delete a chemical, use 'buf chemical -d <chemical_name>'. By default, chemical deletion is shallow/incomplete; the same chemical \
+can still be accessed through its other names after one name has been deleted. For example, if 'buf chemical -a 58.44 NaCl salt' was used to \
+add a chemical to our library, and then the name 'NaCl' was deleted with 'buf chemical -d NaCl', the name 'salt' would still be bound to a molar mass
+of 58.44 in your chemical library. To delete a chemical entirely (i.e. delete all its names), use the '--complete' option. Using the example \
+above, 'buf chemical -d NaCl --complete' would remove both the names 'NaCl' and 'salt' from our chemical library. To skip the program \
+asking you to confirm your decision, use the '--confirm' option.
+
+To view information about a specific chemical (its molar mass and additional names), use 'buf chemical <chemical_name>'. To view your entire \
+chemical library, use 'buf chemical'.
+"""
 
 chemical_library_file = os.path.join(os.path.dirname(__file__), "../library/chemicals.txt")
 
@@ -40,6 +65,24 @@ def chemical(options : dict):
     else:
         display_chemical_library()
 
+# --------------------------------------------------------------------------------
+# --------------------------CHEMICAL DEFINITION AND CREATION----------------------
+# --------------------------------------------------------------------------------
+
+class Chemical:
+    def __init__(self, molar_mass: float, names: list):
+        self.molar_mass = molar_mass
+        self.names = names
+
+    def __repr__(self):
+        # TODO: replace the name code with list_print
+        string = str(self.molar_mass)
+        for name in self.names:
+            string += " " + name
+        return string
+
+    def __eq__(self, other):
+        return self.molar_mass == other.molar_mass and set(self.names) == set(other.names)
 
 # TODO: make sure names cannot be whitespace.
 def make_safe_chemical(molar_mass : str, names : list, chemical_library: dict = None):
@@ -64,23 +107,11 @@ def make_safe_chemical(molar_mass : str, names : list, chemical_library: dict = 
 
     return Chemical(molar_mass, names)
 
+# --------------------------------------------------------------------------------
+# --------------------------------ADDING CHEMICALS--------------------------------
+# --------------------------------------------------------------------------------
 
-class Chemical:
-    def __init__(self, molar_mass: float, names: list):
-        self.molar_mass = molar_mass
-        self.names = names
-
-    def __repr__(self):
-        # TODO: replace the name code with list_print
-        string = str(self.molar_mass)
-        for name in self.names:
-            string += " " + name
-        return string
-
-    def __eq__(self, other):
-        return self.molar_mass == other.molar_mass and set(self.names) == set(other.names)
-
-def add_single_chemical(molar_mass, names):
+def add_single_chemical(molar_mass: str, names: list):
     new_chemical = make_safe_chemical(molar_mass, names)
     with open(chemical_library_file, "a") as file:
         file.write(str(new_chemical) + "\n")
@@ -131,8 +162,65 @@ def add_chemicals_from_file(filename : str):
         for new_chemical in new_chemical_objects:
             file.write(str(new_chemical) + "\n")
 
-    # TODO: delete this?
     print(f"Added the following chemicals to your library:", *new_chemical_names)
+
+# --------------------------------------------------------------------------------
+# ----------------------READING/WRITING TO CHEMICAL LIBRARY-----------------------
+# --------------------------------------------------------------------------------
+
+# TODO: change type hinting to sequence of strings.
+def nickname_chemical(existing_chemical_name: str, new_names: list):
+    chemical_library = load_chemicals()
+
+    if existing_chemical_name not in chemical_library:
+        print(f"Name error: '{existing_chemical_name}' does not exist in chemical library.")
+        exit()
+
+    for new_name in new_names:
+        if new_name in chemical_library:
+            print(f"Name error: '{new_name}' already exists in chemical library.")
+            exit()
+
+    chemical_object = chemical_library[existing_chemical_name]
+
+    chemical_object.names += new_names
+
+    save_chemical_library(chemical_library)
+
+# TODO: print out to user that the chemicals have been deleted?
+def delete_chemical(chemical_name: str, complete_deletion: bool = False, prompt_for_confirmation: bool = True):
+    chemical_library = load_chemicals()
+
+    if chemical_name not in chemical_library:
+        print(f"Chemical not found: '{chemical_name}' not found in chemical library.")
+        exit()
+
+    chemical_object = chemical_library[chemical_name]
+
+    if complete_deletion:
+        names = chemical_object.names
+
+        if prompt_for_confirmation:
+            print("You are about to delete the following chemicals from your library:", *names)
+            user_input.confirm()
+
+        for name in names:
+            del (chemical_library[name])
+
+    else:
+
+        if prompt_for_confirmation:
+            print(f"You are about to delete '{chemical_name}' from your chemical library.")
+            user_input.confirm()
+
+        chemical_object.names.remove(chemical_name)
+        del (chemical_library[chemical_name])
+
+    save_chemical_library(chemical_library)
+
+# --------------------------------------------------------------------------------
+# ------------------------NICKNAMING AND DELETING CHEMICALS-----------------------
+# --------------------------------------------------------------------------------
 
 def save_chemical_library(chemical_library: dict):
     unique_chemical_objects = []
@@ -162,17 +250,25 @@ def load_chemicals():
 
     return chemicals
 
+def reset():
+    with open(chemical_library_file, "w") as file:
+        pass
 
-def display_chemical_information(name):
-    chemicals = load_chemicals()
-    if name in chemicals:
-        chemical = chemicals[name]
+# --------------------------------------------------------------------------------
+# -----------------------------DISPLAYING CHEMICALS-------------------------------
+# --------------------------------------------------------------------------------
+
+
+def display_chemical_information(chemical_name: str):
+    chemical_library = load_chemicals()
+    if chemical_name in chemical_library:
+        chemical = chemical_library[chemical_name]
         # TODO: fancy display
-        print(f"Chemical name: {name}")
+        print(f"Chemical name: {chemical_name}")
 
         # TODO: comment this line
         # TODO: print blank other names line if no other names, or omit line altogether?
-        other_names = [item for item in chemical.names if item != name]
+        other_names = [item for item in chemical.names if item != chemical_name]
         if len(other_names) != 0:
             other_names_string = other_names[0]
             for other_name in other_names[1:]:
@@ -183,8 +279,7 @@ def display_chemical_information(name):
 
     else:
         # TODO: prompt user to add new chemical
-        print(f"Name not found: '{name}' does not currently exist in your chemical library.")
-
+        print(f"Name not found: '{chemical_name}' does not currently exist in your chemical library.")
         exit()
 
 
@@ -201,58 +296,3 @@ def display_chemical_library():
         table.append([chemical_name, chemicals[chemical_name].molar_mass])
 
     print(tabulate.tabulate(table, headers=["Chemical Name", "Molar Mass"], tablefmt="fancy_grid"))
-
-def reset():
-    with open(chemical_library_file, "w") as file:
-        pass
-
-# TODO: change type hinting to sequence of strings.
-def nickname_chemical(existing_chemical_name: str, new_names: list):
-    chemical_library = load_chemicals()
-
-    if existing_chemical_name not in chemical_library:
-        print(f"Name error: '{existing_chemical_name}' does not exist in chemical library.")
-        exit()
-
-    for new_name in new_names:
-        if new_name in chemical_library:
-            print(f"Name error: '{new_name}' already exists in chemical library.")
-            exit()
-
-    chemical_object = chemical_library[existing_chemical_name]
-
-    chemical_object.names += new_names
-
-    save_chemical_library(chemical_library)
-
-
-# TODO: print out to user that the chemicals have been deleted?
-def delete_chemical(chemical_name: str, complete_deletion: bool = False, prompt_for_confirmation: bool = True):
-    chemical_library = load_chemicals()
-
-    if chemical_name not in chemical_library:
-        print(f"Chemical not found: '{chemical_name}' not found in chemical library.")
-        exit()
-
-    chemical_object = chemical_library[chemical_name]
-
-    if complete_deletion:
-        names = chemical_object.names
-
-        if prompt_for_confirmation:
-            print("You are about to delete the following chemicals from your library:", *names)
-            user_input.confirm()
-
-        for name in names:
-            del(chemical_library[name])
-
-    else:
-
-        if prompt_for_confirmation:
-            print(f"You are about to delete '{chemical_name}' from your chemical library.")
-            user_input.confirm()
-
-        chemical_object.names.remove(chemical_name)
-        del(chemical_library[chemical_name])
-
-    save_chemical_library(chemical_library)
