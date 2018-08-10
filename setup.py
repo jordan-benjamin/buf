bf58882f48ba486fff165e9722f0188a2902572e
Jordan Benjamin <jordanjtck@gmail.com>
2018-08-10 18:39:43 -0400

    Adding License text

--- setup.py
+++ setup.py
@@ -4,20 +4,25 @@ from setuptools import setup, find_packages
 with open("README.rst", "r") as file:
     long_description = file.read()
 
+with open("LICENSE", "r") as file:
+      license_text = file.read()
+
 setup(name='buf',
-      version='1.0',
+      version='1.0.0a1',
       description='For easily making chemical buffers and solutions',
       long_description= long_description,
-      # TODO: more classifiers? license?
+      long_description_content_type = "text/x-rst",
       classifiers=[
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
       ],
-      keywords='buf chemistry solution salt',
+      keywords='buf chemistry buffer solution salt',
       # TODO: add URL
       author='Jordan Juravsky',
       author_email='jordan@mindcharger.com',
-      # TODO: license kwarg
-      packages=find_packages(exclude=["tests"]),
-      install_requires=['docopt', 'tabulate'],
+      license = license_text,
+      packages=find_packages(),
+      install_requires=['docopt==0.6.2', 'tabulate==0.8.2'],
       entry_points = {"console_scripts" : ["buf=buf.main:main"]},
       include_package_data = True)
