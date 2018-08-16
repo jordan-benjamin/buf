8924eecc448cc4a49cb5346ca23f293e5089a9eb
Jordan Benjamin <jordanjtck@gmail.com>
2018-08-17 02:29:16 +0300

    Added to instructions docstring, explaining that buffer volumes could be specified in more than litres, showing all the valid units.

--- buf/commands/make.py
+++ buf/commands/make.py
@@ -15,13 +15,15 @@ to make a solution/buffer of a given volume.
 
 To make a solution using a previously defined recipe (see 'buf help recipe' for more information), \
 use 'buf make <volume> <recipe_name>'. For example, to make 2L of a buffer named 'wash', \
-(where 'wash' has already been defined with 'buf recipe -a'), use 'buf make 2L wash'. 
+(where 'wash' has already been defined with 'buf recipe -a'), use 'buf make 2L wash'. The volume of the buffer \
+does not need to be specified only in litres, for example 'buf make 250mL wash' would be acceptable as well. Currently,
+valid volume units are: """ + " ".join(unit.volume_units.get_symbols()) + """
 
 Alternatively, one can define a solution on the spot, with 'buf make <volume> (<concentration> \
 <chemical_name>)...'. For example, 'buf make 0.5L 300mM NaCl 10% glycerol'. \
 Note that in this case, the molar mass of NaCl must already be stored in your chemical library. 
 
-* Note: if one wishes to copy and paste the table outputted by 'buf make' (for example, into a text file to print), \
+* Note: if one wishes to copy and paste a table outputted by 'buf make' (for example, into a text file to print), \
 make sure that one uses the font 'New Courier', in order for the table to be formatted properly. 
 """
 
